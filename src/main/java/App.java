2334abd3032dc6b9c72be990c3225e72b1767b17
mrmiha009 <pfc.cska.rfs@ya.ru>
2023-04-07 10:40:16 +0300

    работает

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,16 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
+        HelloWorld beanHW1 =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        System.out.println(beanHW1.getMessage());
+        HelloWorld beanHW2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        Cat beanCat1 =
+                (Cat) applicationContext.getBean("cat");
+        Cat beanCat2 =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println(beanHW1 == beanHW2);
+        System.out.println(beanCat1 == beanCat2);
     }
 }
